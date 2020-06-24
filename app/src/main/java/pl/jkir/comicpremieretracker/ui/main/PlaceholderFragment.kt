dcd9c8812f8fe8d1c360995159e799d47fe33fd0
Jerzy Kiryczuk <jurkirk@gmail.com>
2020-06-24 19:24:27 +0200

    Premiere-List Code cleanup; add diffrent Fragment to tabview

--- app/src/main/java/pl/jkir/comicpremieretracker/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/pl/jkir/comicpremieretracker/ui/main/PlaceholderFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_main.*
 import pl.jkir.comicpremieretracker.R
@@ -18,13 +16,12 @@ import pl.jkir.comicpremieretracker.R
  */
 class PlaceholderFragment : Fragment() {
 
-    private lateinit var pageViewModel: PageViewModel
+    private lateinit var viewModel: PageViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
-            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
-        }
+        viewModel = ViewModelProvider(this).get(PageViewModel::class.java)
+        viewModel.setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
     }
 
     override fun onCreateView(
@@ -32,7 +29,7 @@ class PlaceholderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
-        pageViewModel.text.observe(viewLifecycleOwner, Observer<String> {
+        viewModel.text.observe(viewLifecycleOwner, Observer<String> {
         })
         return root
     }
@@ -41,7 +38,7 @@ class PlaceholderFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         premiere_list.apply {
             layoutManager = LinearLayoutManager(activity)
-            adapter = PremiereListAdapter(pageViewModel.getMockup())
+            adapter = PremiereListAdapter(viewModel.getMockup())
         }
     }
 
---
+++ app/src/main/java/pl/jkir/comicpremieretracker/ui/main/WatchlistViewModel.kt
@@ -0,0 +1,8 @@
+package pl.jkir.comicpremieretracker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class WatchlistViewModel : ViewModel() {
+
+    fun getMessage() = "Hello Other Fragment"
+}
